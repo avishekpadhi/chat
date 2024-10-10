bf14579e9fcc41dbd3bb719bc7aea51d8c1fe2c7
abhishekpadhi <abhishekpadhi69@gamil.com>
2024-10-10 15:03:22 +0530

    Made some folder structure changes

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-
+import { ChatState } from "../Context/ChatProvider";
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const history = useHistory();
+  const { user, setUser } = ChatState();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -21,6 +22,7 @@ export default function Login() {
       });
 
       localStorage.setItem("userInfo", JSON.stringify(response.data));
+      setUser(JSON.stringify(response.data));
       history.push("/chat");
     } catch (err) {
       setError("Invalid email or password");
@@ -40,14 +42,14 @@ export default function Login() {
                   href="#"
                   className="flex cursor-pointer items-center gap-2 text-indigo-500 no-underline hover:text-indigo-500"
                 >
-                  <span className="flex-shrink-0 text-3xl font-black lowercase tracking-tight opacity-100">
-                    Futurism.
+                  <span className="flex-shrink-0 text-3xl font-black  tracking-tight opacity-100">
+                    Pulse.
                   </span>
                 </a>
               </div>
 
               <h4 className="mb-2 font-medium text-gray-700 xl:text-xl">
-                Welcome to futurism!
+                Welcome to pulse!
               </h4>
               <p className="mb-6 text-gray-500">
                 Please sign-in to access your account
@@ -68,7 +70,6 @@ export default function Login() {
                     className="block w-full cursor-text appearance-none rounded-md border border-gray-400 bg--100 py-2 px-3 text-sm outline-none focus:border-indigo-500 focus:bg-white focus:text-gray-600 focus:shadow"
                     id="email"
                     name="email-username"
-                    autocomplete="email"
                     placeholder="Enter your email or username"
                     autoFocus=""
                     value={email}
@@ -97,25 +98,12 @@ export default function Login() {
                       className="relative block flex-auto cursor-text appearance-none rounded-md border border-gray-400 bg--100 py-2 px-3 text-sm outline-none focus:border-indigo-500 focus:bg-white focus:text-gray-600 focus:shadow"
                       name="password"
                       placeholder="············"
-                      autocomplete="current-password"
                       value={password}
                       onChange={(e) => setPassword(e.target.value)}
                     />
                   </div>
                 </div>
-                <div className="mb-4">
-                  <div className="block">
-                    <input
-                      className="mt-1 mr-2 h-5 w-5 appearance-none rounded border border-gray-300 bg-contain bg-no-repeat align-top text-black shadow checked:bg-indigo-500 focus:border-indigo-500 focus:shadow"
-                      type="checkbox"
-                      id="remember-me"
-                      defaultChecked
-                    />
-                    <label className="inline-block" htmlFor="remember-me">
-                      Remember Me
-                    </label>
-                  </div>
-                </div>
+
                 <div className="mb-4">
                   <button
                     className="grid w-full cursor-pointer select-none rounded-md border border-indigo-500 bg-indigo-500 py-2 px-5 text-center align-middle text-sm text-white shadow hover:border-indigo-600 hover:bg-indigo-600 hover:text-white focus:border-indigo-600 focus:bg-indigo-600 focus:text-white focus:shadow-none"
@@ -128,7 +116,7 @@ export default function Login() {
               </form>
 
               <p className="mb-4 text-center">
-                New on futurism?
+                New on Pulse?
                 <a
                   href="/signup"
                   className="cursor-pointer text-indigo-500 no-underline hover:text-indigo-500"
